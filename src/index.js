de496d7c1031aa0260b6b6433b00c189c40c3aad
JonashGana <jonashtiglao@gmail.com>
2024-09-29 21:12:07 +0800

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles/style.css";
+import { eventHandlers } from "./components/eventHandlers";
+import formSubmission from "./components/formSubmission";
+
+const initialLoad = () => {
+  eventHandlers();
+  formSubmission();
+};
+
+window.onload = initialLoad();
