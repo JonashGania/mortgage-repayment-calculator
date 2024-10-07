86e82770ffc69bf3715f9ffaa23d1956e87e6c36
JonashGana <jonashtiglao@gmail.com>
2024-10-07 19:59:44 +0800

    added typescript-eslint/parser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import typescriptParser from "@typescript-eslint/parser";
 
 export default [
   {
     ignores: ["dist/", "webpack.config.js", "src/styles/**/*.css"],
   },
   { files: ["src/**/*.{js,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.browser, parser: typescriptParser } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
 ];
